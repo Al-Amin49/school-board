69325d55b747e5ecb2b03c44d817aa3e92b44c47
Al-Amin49 <alamin496832@gmail.com>
2024-11-19 00:52:58 +0600

    navbar added

--- src/app/(dashboardLayout)/layout.tsx
+++ src/app/(dashboardLayout)/layout.tsx
@@ -1,5 +1,9 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,18 +18,24 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <div className="h-screen flex ">
+      {/* left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2 "
+        >
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchoolBoard</span>
+        </Link>
+        <Menu />
+      </div>
 
-<div className="h-screen flex">
-    {/* left */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-500">
-left
+      {/* right */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:[86%] bg-[#F7F8FA] overflow-scroll flex flex-col ">
+        <Navbar />
+        {children}
+      </div>
     </div>
-
-
-    {/* right */}
-    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:[86%] bg-[#F7F8FA] ">right</div>
-
-</div>
-
-  )
+  );
 }
