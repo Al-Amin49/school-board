c57e536802bd6cfe4788121b012d513f3598443e
Al-Amin49 <alamin496832@gmail.com>
2024-11-28 18:37:07 +0600

    feat:card added

--- src/app/(dashboardLayout)/admin/page.tsx
+++ src/app/(dashboardLayout)/admin/page.tsx
@@ -1,9 +1,23 @@
+import UserCard from '@/components/UserCard';
 import React from 'react';
 
 const AdminPage = () => {
     return (
-        <div>
-            <h3>This is admin page</h3>
+        <div className='p-4 flex gap-4 flex-col md:flex-row'>
+
+            {/* left */}
+            <div className="w-full lg:w-2/3">
+         <div className='flex gap-4 justify-between flex-1 flex-wrap'>
+         <UserCard type="student"></UserCard>
+        <UserCard type="teacher"></UserCard>
+        <UserCard type="parent"></UserCard>
+        <UserCard type="staff"></UserCard>
+         </div>
+            </div>
+      
+            {/* Right */}
+            <div className="w-full lg:w-1/3">R</div>
+            
         </div>
     );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      colors: {
+        skyBlue: "#88E1F2",          // Bright sky blue
+        skyBlueLight: "#DAF7FE",     // Light sky blue
+        royalPurple: "#A78BFA",      // Soft royal purple
+        lavender: "#EAE4FF",         // Light lavender
+        goldenYellow: "#FFD700",     // Rich golden yellow
+        paleYellow: "#FFF9E5",       // Pale yellow
+      },
+      
     },
   },
   plugins: [],
