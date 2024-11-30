404ecf86d3c623bced9bd019e8d51d22c683df19
Al-Amin49 <alamin496832@gmail.com>
2024-11-30 22:56:21 +0600

    feat: student and attendacne chart created with rechart

--- src/app/(dashboardLayout)/admin/page.tsx
+++ src/app/(dashboardLayout)/admin/page.tsx
@@ -1,25 +1,38 @@
-import UserCard from '@/components/UserCard';
-import React from 'react';
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 const AdminPage = () => {
-    return (
-        <div className='p-4 flex gap-4 flex-col md:flex-row'>
-
-            {/* left */}
-            <div className="w-full lg:w-2/3">
-         <div className='flex gap-4 justify-between flex-1 flex-wrap'>
-         <UserCard type="student"></UserCard>
-        <UserCard type="teacher"></UserCard>
-        <UserCard type="parent"></UserCard>
-        <UserCard type="staff"></UserCard>
-         </div>
-            </div>
-      
-            {/* Right */}
-            <div className="w-full lg:w-1/3">R</div>
-            
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* left */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        <div className="flex gap-4 justify-between flex-1 flex-wrap">
+          <UserCard type="student"></UserCard>
+          <UserCard type="teacher"></UserCard>
+          <UserCard type="parent"></UserCard>
+          <UserCard type="staff"></UserCard>
         </div>
-    );
+
+        {/* Middle chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+      {/* COUNT CHART */}
+      <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            {/* <AttendanceChart /> */}
+            <AttendanceChart/>
+          </div>
+        </div>
+      </div>
+
+      {/* Right */}
+      <div className="w-full lg:w-1/3">R</div>
+    </div>
+  );
 };
 
 export default AdminPage;
